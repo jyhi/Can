488e2597c1efc559b67f527f1f58277b2da24ce5
Minyong Li <ml10g20@soton.ac.uk>
2021-08-21 13:31:04 +0100

    CanConfiguration: remove

--- src/main/scala/uk/ac/soton/ecs/can/config/CanConfiguration.scala
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2021 Minyong Li <ml10g20@soton.ac.uk>
-// SPDX-License-Identifier: CERN-OHL-W-2.0
-
-package uk.ac.soton.ecs.can.config
-
-case class CanConfiguration(
-    core: CanCoreConfiguration
-)
